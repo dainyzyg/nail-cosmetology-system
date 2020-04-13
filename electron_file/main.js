6ce60f64d432b519e6806e3de8d3bb67882b4957
dainyzyg <470067721@qq.com>
2020-04-13 16:15:42 +0800

    upgrade prettier

--- electron_file/main.js
+++ electron_file/main.js
@@ -1,6 +1,10 @@
-const environment = 'product' // product dev
+let environment = 'product' // product dev
 const openDevTools = false
 const electron = require('electron')
+
+if (process.argv.find((x) => x == '--dev')) {
+  environment = 'dev'
+}
 
 const { ipcMain, webContents } = require('electron')
 ipcMain.on('notice-fee-screen', (event, arg) => {
@@ -40,13 +44,15 @@ app.setPath('userData', path.join(__dirname, '../../userData'))
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-const isSecondInstance = app.makeSingleInstance((commandLine, workingDirectory) => {
-  // Someone tried to run a second instance, we should focus our window.
-  if (mainWindow) {
-    if (mainWindow.isMinimized()) mainWindow.restore()
-    mainWindow.focus()
+const isSecondInstance = app.makeSingleInstance(
+  (commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
   }
-})
+)
 
 if (isSecondInstance) {
   app.quit()
@@ -60,7 +66,7 @@ function showMessage(message) {
     message: message,
     buttons: ['是', '否']
   }
-  dialog.showMessageBox(options, function(index) {})
+  dialog.showMessageBox(options, function (index) {})
 }
 
 function registerProtocol() {
@@ -126,7 +132,7 @@ function createWindow() {
   }
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -141,7 +147,7 @@ function createWindow() {
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
@@ -149,7 +155,7 @@ app.on('window-all-closed', function() {
   }
 })
 
-app.on('activate', function() {
+app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
